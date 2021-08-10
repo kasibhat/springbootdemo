794bf780fa8325e2261eb6ffbfd074142ae82df6
kasibhat@gmail.com <Amma@1961>
2021-08-10 16:21:22 +0530

    ewr

--- src/main/java/com/example/springbootdockerdemo/SpringbootdockerdemoApplication.java
+++ src/main/java/com/example/springbootdockerdemo/SpringbootdockerdemoApplication.java
@@ -2,15 +2,15 @@ package com.example.springbootdockerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-java.util.*;
+import java.util.*;
 
 @SpringBootApplication
 public class SpringbootdockerdemoApplication {
 
 	public static void main(String[] args) {
 		List<String> alist = new ArrayList<String>();
-		aList.add("efer");
-		aList.add("efe2r");
+		alist.add("efer");
+		alist.add("efe2r");
 		System.out.println("om sai ram");
 		SpringApplication.run(SpringbootdockerdemoApplication.class, args);
 	}
